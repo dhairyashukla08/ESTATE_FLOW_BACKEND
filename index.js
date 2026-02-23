d45cc67b36871c5b00ac93ce3d41cd524dd93161
dhairyashukla08 <dhairyvshukla@gmail.com>
2026-02-23 23:41:48 +0530

    updated the index.js with updated netlify link

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173","https://estate-flow-frontend.netlify.app/"],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   credentials: true,
 };
